71b0a060e18f3fd4f0d80a7a80b209f55a246ccb
ivan <ivan@es.darktech.org>
2016-03-26 14:59:12 +0100

    Modificado getParserService para aceptar una lista de censos y otra de formatos para generar las cartas

--- src/main/java/es/uniovi/asw/util/ServicesFactory.java
+++ src/main/java/es/uniovi/asw/util/ServicesFactory.java
@@ -1,25 +1,28 @@
-/**
- * 
- */
 package es.uniovi.asw.util;
 
-import es.uniovi.asw.dbupdate.DBUpdate;
-import es.uniovi.asw.dbupdate.DBUpdateService;
-import es.uniovi.asw.parser.Parser;
-import es.uniovi.asw.parser.ParserService;
+import java.io.IOException;
+import java.util.List;
+
+import es.uniovi.asw.dbupdate.impl.InsertImpl;
+import es.uniovi.asw.parser.ReadCensus;
+import es.uniovi.asw.parser.impl.ReadCensusImpl;
+import es.uniovi.asw.reportwriter.WriteReport;
+import es.uniovi.asw.reportwriter.impl.WriteReportImpl;
 
 /**
  * @author ivan
- *
  */
 public class ServicesFactory {
 	
-	public static ParserService getParserService() {
-		return new Parser();
+	public static ReadCensus getParserService(List<String> fileNames, List<String> formats) {
+		return new ReadCensusImpl(fileNames, formats);
 	}
 	
-	public static DBUpdateService getDBUpdateService() {
-		return new DBUpdate();
+	public static InsertImpl getDBUpdateService() {
+		return new InsertImpl();
 	}
 
+	public static WriteReport getWriteReportService() throws IOException {
+		return WriteReportImpl.getInstance();
+	}
 }

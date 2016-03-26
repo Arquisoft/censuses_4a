69c96164d0cf64b5fa7eaf2d9fb94643ee06b940
ivan <ivan@es.darktech.org>
2016-03-26 14:54:11 +0100

    Reescrito, en vez de mostrar un menú interactivo acepta argumentos: una lista de censos y opciones de formato

--- src/main/java/es/uniovi/asw/util/Console.java
+++ src/main/java/es/uniovi/asw/util/Console.java
@@ -1,123 +1,96 @@
-/**
- * 
- */
 package es.uniovi.asw.util;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-
-import es.uniovi.asw.parser.ports.WreportR;
-import es.uniovi.asw.reportwriter.ReportWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author ivan
- *
  */
 public class Console {
 
-	private static WreportR reportR;
-	
 	/**
 	 * @param args
 	 */
-	public static void start(String[] args) {
+	public static void start(String... args) {
 		
-		int opcion;
-		reportR = new WreportR(new ReportWriter());
+		List<String> fileNames = new ArrayList<String>();
+		List<String> formats = new ArrayList<String>();
+		List<String> errors = new ArrayList<String>();
 		
-		while ((opcion = menu()) != 2) {
-			if (opcion == 1) {
-				String fileName = readFileName();
-				String format = readFormat();
-				ServicesFactory.getParserService().readCensus(fileName, format);
+		boolean printHelp = false;
+		
+		for (int i = 0; i < args.length; i++) {
+			
+			if (args[i].equals("-h") || args[i].equals("--help")) {
+				printHelp = true;
+				break;
 			}
+			
+			else if (args[i].contains(".xlsx") || args[i].contains(".csv") || args[i].contains(".txt")) {
+				if (!fileNames.contains(args[i])) fileNames.add(args[i]);
+			} 
+			
+			else if (args[i].equals("-p") || args[i].equals("--pdf")) {
+				if (!formats.contains("pdf")) formats.add("pdf");
+			}
+			
+			else if (args[i].equals("-w") || args[i].equals("--word")) {
+				if (!formats.contains("word")) formats.add("word");
+			}
+			
+			else if (args[i].equals("-t") || args[i].equals("--txt")) {
+				if (!formats.contains("txt")) formats.add("txt");
+			}
+			
+			else {
+				errors.add(args[i]);
+			}
+			
 		}
 		
-	}
-	
-	public static int menu() {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int option = 0;
-
-        System.out.println("\n Censo Electoral");
-        System.out.println(" -------------------------------");
-        System.out.println(" 1 - Leer censo y generar cartas");
-        System.out.println(" -------------------------------");
-        System.out.println(" 2 - Salir");
-
-        try{
-            option = Integer.parseInt(br.readLine());
-        }
-        
-        catch(NumberFormatException nfe){
-            System.out.println("Debes seleccionar un numero entre 1 y 2");
-        }
-        
-        catch (IOException e) {
-        	reportR.log(
-					"ERROR - El usuario ha realizado una accion inesperada en la consola"
-				);
+		if (printHelp) {
+			printHelp();
+			return;
 		}
-        
-        return option;    
-    }
-	
-	public static String readFileName() {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-        System.out.print("Nombre del archivo que contiene el censo: ");
-        String fileName = "";
-        
+		if (!errors.isEmpty()) {
+			System.out.print("Unrecognized parameters: ");
+			for (int i = 0; i < errors.size(); i++) {
+				System.out.print(errors.get(i) + " ");
+			}
+			System.out.println("\n");
+			printHelp();
+			return;
+		}
+		
+		if (fileNames.isEmpty()) {
+			System.out.println("Error: no files specified\n");
+			printHelp();
+			return;
+		}
+		
+		if (formats.isEmpty())
+			formats.add("txt");
+		
 		try {
-			fileName = br.readLine();
+			ServicesFactory.getParserService(fileNames, formats).read();
 		}
 		
 		catch (IOException e) {
-			reportR.log(
-					"ERROR - El usuario ha introducido una opcion incorrecta en la consola"
-				);
+			System.out.println(e.getMessage());
 		}
-        
-        return fileName;    
-    }
+	}
 	
-	public static String readFormat() {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int option = 0;
-
-		System.out.println("\n Formato de las cartas");
-        System.out.println(" ----------------------------");
-        System.out.println(" 1 - En formato PDF");
-        System.out.println(" 2 - En formato Word");
-        System.out.println(" 3 - En Texto Plano");
-        System.out.println(" ----------------------------");
-
-        try{
-            option = Integer.parseInt(br.readLine());
-        }
-        
-        catch(NumberFormatException e){
-            System.out.println("Debes seleccionar un numero entre 1 y 3");
-        }
-        
-        catch (IOException e) {
-        	reportR.log(
-					"ERROR - El usuario ha realizado una accion inesperada en la consola"
-				);
-		}
-        
-        switch(option) {
-        	case 1:
-        		return "pdf";
-        	case 2:
-        		return "word";
-        	default:
-        		return "txt";
-        }
-    }
+	private static void printHelp() {
+		System.out.println("Usage: censusreader FILE1 FILE2... PARAM1 PARAM2...");
+		System.out.println("\nRead censuses files (xlsx|csv|txt) and generate notification letters in PDF, MS Word and Plain Text formats.");
+		System.out.println("\nParameters:\n");
+		System.out.println("    -p, --pdf            Generate letters in PDF format");
+		System.out.println("    -w, --word           Generate letters in Word format");
+		System.out.println("    -t, --txt            Generate letters in Plain Text format");
+		System.out.println("    -h, --help           Show this help");
+		System.out.println("\nIf no format is specified the letters will be generated in Plan Text.");
+	}
 
 }

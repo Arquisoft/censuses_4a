655b693e40a81063e30b80d1be4469c8b40fb280
Iván <ivan_ma@telecable.es>
2016-03-26 15:19:19 +0100

    Adaptaciones al nuevo diseño. Los errores se gestionan mediante excepciones. Los mensajes de error incluyen el archivo y la línea que los ha generado

--- src/main/java/es/uniovi/asw/parser/ports/InsertR.java
+++ src/main/java/es/uniovi/asw/parser/ports/InsertR.java
@@ -1,20 +1,28 @@
 package es.uniovi.asw.parser.ports;
 
-import java.util.List;
+import java.util.Map;
 
-import es.uniovi.asw.dbupdate.interfaces.Insert;
-import es.uniovi.asw.dbupdate.model.Voter;
-import es.uniovi.asw.util.ServicesFactory;
+import es.uniovi.asw.model.Voter;
+import es.uniovi.asw.util.DataValidator;
+import es.uniovi.asw.util.ReadCensusException;
 
-public class InsertR implements Insert {
+public class InsertR {
 
-	@Override
-	public void insert(List<Voter> voters) {
+	public static Voter verify(Map<String, Object> voter) throws ReadCensusException {
 		
-		// Verificar que cada votante tiene todos los campos
-		// y que DNI e email lo son realmente mediante mascaras
+		if (voter.get("nif") == null || !DataValidator.isNifValid(voter.get("nif").toString()))
+			throw new ReadCensusException("[ERROR] [" + voter.get("file") + ":" + voter.get("line") + "] El NIF del usuario no tiene el formato correcto");
 		
-		ServicesFactory.getDBUpdateService().insertVoters(voters);
+		if (voter.get("name") == null || voter.get("name").toString().equals(""))
+			throw new ReadCensusException("[ERROR] [" + voter.get("file") + ":" + voter.get("line") + "] El usuario " + voter.get("nif") + " no tiene nombre");
+		
+		if (voter.get("email") == null || !DataValidator.isEmailValid(voter.get("email").toString()))
+			throw new ReadCensusException("[ERROR] [" + voter.get("file") + ":" + voter.get("line") + "] El email del usuario " + voter.get("nif") + " no es válido");
+		
+		if (voter.get("code") == null || !DataValidator.isNumberValid(voter.get("code").toString()))
+			throw new ReadCensusException("[ERROR] [" + voter.get("file") + ":" + voter.get("line") + "] El colegio electoral del usuario " + voter.get("nif") + " no es válido");
+
+		return new Voter(voter.get("name").toString(), voter.get("email").toString(), voter.get("nif").toString(), Integer.parseInt(voter.get("code").toString()), voter.get("password").toString());
 		
 	}
 

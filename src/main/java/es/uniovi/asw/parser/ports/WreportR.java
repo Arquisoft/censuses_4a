bcce1e733e181ec6fefe07dc4a06b7dd72d2d762
Iván <ivan_ma@telecable.es>
2016-03-26 15:21:26 +0100

    Modificaciones para cumplir con la arquitectura. El puerto WreportR sólo valida el mensaje de error

--- src/main/java/es/uniovi/asw/parser/ports/WreportR.java
+++ src/main/java/es/uniovi/asw/parser/ports/WreportR.java
@@ -1,26 +1,12 @@
 package es.uniovi.asw.parser.ports;
 
-import es.uniovi.asw.reportwriter.ports.WreportP;
-import es.uniovi.asw.reportwriter.ReportWriter;
-import es.uniovi.asw.reportwriter.interfaces.WriteReport;
-
-public class WreportR implements WriteReport {
+public class WreportR {
 	
-	ReportWriter rw;
-	
-	public WreportR(ReportWriter rw) {
-	 this.rw = rw; 	
-	}
-	
-	@Override
-	public void log(String text) {
-		
-		WreportP wreportP = rw.getLogger();
-		
+	public static boolean verify(String text) {
 		if (text != null && !text.equals(""))
-			wreportP.log(text);
+			return true;
 		else
-			wreportP.log("ERROR - El texto a escribir en el log esta vacio.");
+			return false;
 	}
 	
 }

0a3f11d1c1a0fa01192f1ca1077b7fc386a74d7d
Iván <ivan_ma@telecable.es>
2016-04-08 03:57:25 +0200

    Añadido un método que comprueba si el directorio Letters existe y en caso de no ser así, lo crea

--- src/main/java/es/uniovi/asw/parser/writers/LetterWriterFactory.java
+++ src/main/java/es/uniovi/asw/parser/writers/LetterWriterFactory.java
@@ -2,11 +2,14 @@ package es.uniovi.asw.parser.writers;
 
 import es.uniovi.asw.util.ReadCensusException;
 
+import java.io.File;
+
 public class LetterWriterFactory {
 
 	public static LetterWriter create(String format) throws ReadCensusException {
 
 		LetterWriter writer = null;
+		createLettersFolder();
 		
 		if (format.equals("pdf"))
 			writer = new PDFWriter();
@@ -19,8 +22,15 @@ public class LetterWriterFactory {
 
 		else
 			throw new ReadCensusException("[ERROR] El formato para generar las cartas (" + format + ") no está soportado");
-		
+
 		return writer;
+	}
+
+	private static void createLettersFolder() {
+		File file = new File("Letters");
+
+		if (!file.exists())
+			file.mkdir();
 	}
 	
 }
